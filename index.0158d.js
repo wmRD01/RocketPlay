2536d19df477846ce161d0c504170f27899c8bf6
JaiJai <jianliang@ceis.tw>
2022-09-15 16:30:37 +0800

    0.0.4_NotLang

--- index.0158d.js
+++ index.0158d.js
@@ -1,4 +1,4 @@
-System.register(["./application.b2528.js"], function (_export, _context) {
+System.register(["./application.79158.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -81,8 +81,8 @@ System.register(["./application.b2528.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationB2528Js) {
-      createApplication = _applicationB2528Js.createApplication;
+    setters: [function (_application79158Js) {
+      createApplication = _application79158Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
