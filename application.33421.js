f26a4b75eea07cad3d20727d3f6373f4727047d0
JaiJai <jianliang@ceis.tw>
2022-09-21 17:41:07 +0800

    0.0.7

--- application.33421.js
+++ application.33421.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.9c1fd.json';
+      var settings = 'src/settings.a8e94.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
