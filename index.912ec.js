3ebf78bfd0853b3c4da39c5e9ed5b3c413fbc76a
JaiJai <jianliang@ceis.tw>
2022-09-22 09:20:05 +0800

    0.0.61

--- index.912ec.js
+++ index.912ec.js
@@ -1,4 +1,4 @@
-System.register(["./application.33421.js"], function (_export, _context) {
+System.register(["./application.7fbc5.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.33421.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application33421Js) {
-      createApplication = _application33421Js.createApplication;
+    setters: [function (_application7fbc5Js) {
+      createApplication = _application7fbc5Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
