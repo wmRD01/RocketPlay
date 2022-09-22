1f711a66373530ab22d08e2e24dd73e7fa7984b9
JaiJai <jianliang@ceis.tw>
2022-09-22 10:31:21 +0800

    0.0.65

--- index.87c0d.js
+++ index.87c0d.js
@@ -1,4 +1,4 @@
-System.register(["./application.74a73.js"], function (_export, _context) {
+System.register(["./application.84628.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.74a73.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application74a73Js) {
-      createApplication = _application74a73Js.createApplication;
+    setters: [function (_application84628Js) {
+      createApplication = _application84628Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
