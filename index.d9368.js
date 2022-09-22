af1fbba4600166774be287608d8fb8d5b638d7b2
JaiJai <jianliang@ceis.tw>
2022-09-22 10:22:36 +0800

    0.0.64

--- index.d9368.js
+++ index.d9368.js
@@ -1,4 +1,4 @@
-System.register(["./application.ffd54.js"], function (_export, _context) {
+System.register(["./application.74a73.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.ffd54.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationFfd54Js) {
-      createApplication = _applicationFfd54Js.createApplication;
+    setters: [function (_application74a73Js) {
+      createApplication = _application74a73Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
