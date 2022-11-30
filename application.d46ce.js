7ffe7e863ea6ec1cd3d666bf9d7ca3e9762d219c
JaiJai <jianliang@ceis.tw>
2022-11-30 11:21:01 +0800

    0.0.151
    1. 修改viewRecode方式，解決循環引用問題

--- application.d46ce.js
+++ application.d46ce.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.e2ba6.json';
+      var settings = 'src/settings.977e2.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.ea5f8.js
+++ index.ea5f8.js
@@ -1,4 +1,4 @@
-System.register(["./application.7f757.js"], function (_export, _context) {
+System.register(["./application.d46ce.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -81,8 +81,8 @@ System.register(["./application.7f757.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application7f757Js) {
-      createApplication = _application7f757Js.createApplication;
+    setters: [function (_applicationD46ceJs) {
+      createApplication = _applicationD46ceJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
